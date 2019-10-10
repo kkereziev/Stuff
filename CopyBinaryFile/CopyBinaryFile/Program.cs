b057153bf8a39fd4c0b6542c4d0ce5b19f7249ca
krumeto97 <krumeto97@abv.bg>
2019-10-10 16:53:57 +0300

    Add files via upload

---
+++ CopyBinaryFile/CopyBinaryFile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.IO;
+namespace _4._Copy_Binary_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new FileStream("CopyMe.png", FileMode.Open, FileAccess.Read))
+            {
+                using (var writer = new FileStream("copiedFile.png", FileMode.Create, FileAccess.Write))
+                {
+                    var buffer = new byte[4096];
+                    var length = 0;
+
+                    while ((length = reader.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        writer.Write(buffer, 0, length);
+                    }
+                }
+            }
+        }
+    }
+}
