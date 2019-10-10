201030afa1f25f57aef4eb1a95ad9843e2d93a27
krumeto97 <krumeto97@abv.bg>
2019-10-10 15:32:23 +0300

    Add files via upload

---
+++ SliceAFile/SliceAFile/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SliceAFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileLength = new FileInfo("./sliceMe.txt");
+            var parts = 4;
+            var partSize = fileLength.Length/parts;
+            var fileNumber = 1;
+            var readBytes = 0;
+            using (var reader = new StreamReader("sliceMe.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var bufferSize = partSize;
+                    if (fileNumber==parts)
+                    {
+                        bufferSize = fileLength.Length - (parts - 1) * partSize;
+                    }
+                    var buffer = new char[bufferSize];
+                    readBytes += reader.Read(buffer, 0, buffer.Length);
+                    using (var writer = new StreamWriter($"./sliced-{fileNumber}.txt"))
+                    {
+                        writer.Write(buffer);
+                    }
+                    if (readBytes==fileLength.Length)
+                    {
+                        break;
+                    }
+                    fileNumber++;
+                }
+            }
+        }
+    }
+}
