e0d978655ff68cbfba3a7f14d943c996e2db275e
krumeto97 <krumeto97@abv.bg>
2019-10-10 16:24:56 +0300

    Add files via upload

---
+++ ZipAndExtract/ZipAndExtract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO.Compression;
+
+namespace ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var zipFile = "myZip.zip";
+            var files = Directory.GetFiles(Environment.CurrentDirectory);
+            var extractFolderPath = @"C:\Users\Krum Kereziev\source\repos\ZipAndExtract\ZipAndExtract\ExtractFolder";
+            using (var archive=ZipFile.Open(zipFile,ZipArchiveMode.Create))
+            {
+                foreach (var item in files)
+                {
+                    archive.CreateEntryFromFile(item, Path.GetFileName(item));
+                }
+            }
+            ZipFile.ExtractToDirectory("myZip.zip", extractFolderPath);
+        }
+    }
+}
